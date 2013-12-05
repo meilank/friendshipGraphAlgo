83696dcb6c5670fa53f1a42cd0d0baca0a964fd6
meilank <meilan.keshava@gmail.com>
2013-12-05 16:04:42 -0500

    Update FriendDriver.java

--- FriendshipGraphAlgo/src/FriendDriver.java
+++ FriendshipGraphAlgo/src/FriendDriver.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class FriendDriver 
+public class FriendDriver
 {
 	static BufferedReader br, file;
 	public static final int studSchool = 1;
@@ -10,8 +10,17 @@ public class FriendDriver
 	public static final int connectors = 4;
 	public static final int quit = 5;
 
-	public static void main(String[] args) throws IOException 
+	static Friend newFriend = new Friend();
+
+	public static void main(String[] args) throws IOException
 	{
+		String string = "sam|y|rutgers";
+		String[] parts = string.split("|");
+		String part1 = parts[0]; // 004
+		String part2 = parts[1]; // 034556
+		System.out.println(parts[0] + " " +parts[1] + " " +parts[2]);
+
+
 		System.out.println("Welcome to the Friendship Graph Algorithm program.");
 		System.out.println("Please input the name of a file that you would like to generate a graph from.");
 		System.out.println("The first line of the file should contain the number (n) of friends in the list.");
@@ -19,14 +28,13 @@ public class FriendDriver
 		System.out.println("The format for a friend who attends a university should be sharlina|y|rutgers.");
 		System.out.println("The format for a friend who does not attend a university should be ellen|n");
 		System.out.println("The lines below that should list out the relationships between your friends.");
-		System.out.println("An example would be sharlina|ellen.  This indicates that sharlina and ellen have a relationship.");
+		System.out.println("An example would be sharlina|ellen. This indicates that sharlina and ellen have a relationship.");
 
-		
+
 		//how to read file in
 		br = new BufferedReader(new InputStreamReader(System.in));
 		file = new BufferedReader(new FileReader(br.readLine()));
 		Scanner scan= new Scanner(file);
-		Friend newFriend = new Friend();
 		newFriend.buildGraph(scan);
 
 		int choice = getChoice();
@@ -37,11 +45,11 @@ public class FriendDriver
 			else
 				switch (choice)
 				{
-					case studSchool: studSchool(); break;
-					case intro: intro(); break;
-					case cliques: cliques(); break;
-					case connectors: connectors(); break;
-					default: break;
+				case studSchool: studSchool(); break;
+				case intro: intro(); break;
+				case cliques: cliques(); break;
+				case connectors: connectors(); break;
+				default: break;
 				}
 			choice = getChoice();
 		}
@@ -51,18 +59,22 @@ public class FriendDriver
 	public static int getChoice() throws IOException
 	{
 		System.out.println();
-		System.out.println(studSchool + ".  Generate a subgraph containing all of your friends that attend a particular school. ");
-		System.out.println(intro + ". EVALUATE polynomial");
-		System.out.println(cliques + ". EVALUATE polynomial");
-		System.out.println(connectors + ". EVALUATE polynomial");
+		System.out.println(studSchool + ". Subgraph");
+		System.out.println(intro + ". Introduction");
+		System.out.println(cliques + ". Cliques");
+		System.out.println(connectors + ". Connectors");
 		System.out.println(quit + ". QUIT");
 		System.out.print("\tEnter choice # => ");
 		return (Integer.parseInt(br.readLine()));
 	}
 
-	public static void studSchool()
+	public static void studSchool() throws IOException
 	{
-
+		System.out.println("Please input the name of the school you would like to see subgraph for.");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String school = in.readLine();
+		newFriend.subGraph(school);
+		//System.out.println(newFriend.subGraph(newFriend.peopleAdjLL, school));
 	}
 
 	public static void intro()
@@ -82,3 +94,5 @@ public class FriendDriver
 
 
 }
+
+
